73e81bbed44220fa9e39ffa6d39cc93483ce579e
kostjavovhc <kostjavovhc@gmail.com>
2023-11-04 19:23:01 +0200

    delete coments

--- dz_11.py
+++ dz_11.py
@@ -24,21 +24,23 @@ def user_error(func):
 
 
 @user_error
-def add_contact(*args): #працює
+def add_contact(*args):
     name = args[0].capitalize()
     phone = args[1]
+    
     try:
         birthday = args[2]
     except:
         IndexError
         birthday = None
+
     record = Record(name, birthday)
     record.add_phone(phone)
     contacts.add_record(record)
     return f"{record.name} was succesfully added to Databook"
 
 @user_error
-def add_phone(*args): #
+def add_phone(*args):
     name = args[0].capitalize()
     phone_to_add = args[1]
     contacts.find(name).add_phone(phone_to_add)
@@ -46,7 +48,7 @@ def add_phone(*args): #
 
 
 @user_error
-def change_phone(*args): #працює
+def change_phone(*args):
     name = args[0].capitalize()
     old_phone = args[1]
     new_phone = args[2]
@@ -55,7 +57,7 @@ def change_phone(*args): #працює
 
 
 @user_error
-def show_num(*arg): #працює
+def show_num(*arg):
     cap_arg = arg[0].capitalize()
     if cap_arg in contacts.data.keys():
         return f"{cap_arg}'s phones: {contacts[cap_arg].phones}"
@@ -64,12 +66,12 @@ def show_num(*arg): #працює
  
 
 @user_error
-def greeting_hello(): #працює
+def greeting_hello():
     return f"Hello!\nI support this commands: {[p for p in COMMANDS.values()]}\nHow can i help you?"
 
 
 @user_error 
-def show_all_nums(): #працює
+def show_all_nums():
    sorted_data = dict(sorted(contacts.data.items()))
    line = "_"*50 + "\n"
    for p in sorted_data.values():
@@ -88,7 +90,7 @@ def days_bd(arg):
 
 
 @user_error
-def show_nums(*args): #працює
+def show_nums(*args):
     num = int(args[0])
     for res in contacts.iterator(num):
         print("_" * 30)
